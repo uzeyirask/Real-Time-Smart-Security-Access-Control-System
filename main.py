6dc218a0216f4d9db654ededa07956a69b902f88
uzeyirask <92611888+uzeyirask@users.noreply.github.com>
2024-11-29 22:24:57 +0300

    Yuz Eslesme, Eslestirme Tamamlandi
    Artik camdan aldigi yuzu taniyor ve bunu imagesteki her bir fotografla for dongusu araciligiyla kontrol ediyor. Benzer yuzu tespit ettiginde true donduruyor.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 from copyreg import pickle
-
+import pickle
 import cv2
-
-from EncodeGenarator import encodeListKnownWithIds, encodeListKnown, ogrenciIds
+import face_recognition
+from face_recognition import face_encodings
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)  # genisligini ayarladim
@@ -19,18 +19,35 @@ for path in modlarinYolList:
 #print(len(modList))
 
 #Encoding dosyasi yuklenme yeri
+print("Encode File Yukleniyor...")
 file = open('EncodeFile.p','rb')
 encodeListKnownWithIds = pickle.load(file)
 file.close()
 encodeListKnown, ogrenciIds = encodeListKnownWithIds
-# print(ogrenciIds)
+#print(ogrenciIds)
+print("Encode File Yuklendi")
+
 
 while True:
     success, img = cap.read()  # kameradan yakalar
 
+    imgS = cv2.resize(img, (0,0),None,0.25,0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    faceCurFrame = face_recognition.face_locations(imgS)
+    encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
+
+
     imgBackround[162:162 + 480,55:55 + 640] = img #
     #cv2.imshow("Webcam", img)  # yukarida gercek zamanli akilli yaziyor
     imgBackround[44:44 + 633,808:808 + 414] = modList[0]
+
+    for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
+        print("Eslesme",matches)
+        print("Distance",faceDis)
+
 
 
     cv2.imshow("Yuz Tanima Ekrani", imgBackround)
